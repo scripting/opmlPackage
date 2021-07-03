9f58d2cbedb254cd422dbc01fc6841f3f14d6424
Dave Winer <dave.winer@gmail.com>
2021-07-03 11:52:27 -0400

    .

--- README.md
+++ README.md
@@ -2,3 +2,31 @@
 
 Node and browser-based code that helps developers support OPML in outliners, processors and renderers.
 
+### Using the Node package
+
+To parse a local OPML file:
+
+const fs = require ("fs");
+
+const opml = require ("opml");
+
+fs.readFile ("states.opml", function (err, opmltext) {
+
+if (err) {
+
+console.log (err.message);
+
+}
+
+else {
+
+opml.parse (opmltext, function (err, theOutline) {
+
+console.log (JSON.stringify (theOutline, undefined, 4));
+
+});
+
+}
+
+});
+
--- opmlpackage.js
+++ opmlpackage.js
@@ -1,10 +1,12 @@
-const myVersion = "0.4.1", myProductName = "opmlPackage"; 
+const myVersion = "0.4.4", myProductName = "opmlPackage"; 
 
 exports.parse = parse; 
+exports.stringify = stringify; 
 
 const utils = require ("daveutils");
 const opmltojs = require ("opmltojs");
 const xml2js = require ("xml2js");
+const opmlToJs = require ("opmltojs");
 
 function isScalar (obj) {
 	if (typeof (obj) == "object") {
@@ -12,7 +14,13 @@ function isScalar (obj) {
 		}
 	return (true);
 	}
-function parse (opmltext, callback) {
+function parse (opmltext, callback) { //returns a JavaScript object with all the info in the opmltext
+	//Changes
+		//1/18/21; 10:21:27 AM by DW
+			//I created an OPML format that added a "subs" attribute to each headline that had subs. This was an error, but was still valid OPML, but it caused this code to fail, because subs was the wrong type. It is always a mistake, if it's possible that your OPML will be converted to a JS object. So I protected against it here, and don't copy an attribute called subs if it's present. It's possible that this fix could cause problems too, btw. The code is in Old School, look for saveDayInOpml. 
+		//4/18/20; 5:43:20 PM by DW
+			//Changed the callback to return the standard format, with an err first, and theOutline second. 
+			//I didn't want to break all the apps that use this as it was configured, but in the future, use this entry point not the one without the error.
 	function addGenerator (theOpml) { //follow the example of RSS 2.0
 		try {
 			theOpml.head.generator = myProductName + " v" + myVersion;
@@ -82,4 +90,27 @@ function parse (opmltext, callback) {
 			callback (undefined, theOutline);
 			}
 		});
+	//xml2js.parseString (opmltext, options, function (err, jstruct) {
+		//if (err) { //4/18/20 by DW
+			//callback (err);
+			//}
+		//else {
+			//var theOutline = {
+				//opml: new Object ()
+				//}
+			//convert (jstruct.opml, theOutline.opml);
+			//addGenerator (theOutline.opml); //8/6/17 by DW
+			//if (isScalar (theOutline.opml.head)) { //8/6/17 by DW
+				//theOutline.opml.head = new Object ();
+				//}
+			//if (isScalar (theOutline.opml.body)) { //8/6/17 by DW
+				//theOutline.opml.body = new Object ();
+				//}
+			//callback (undefined, theOutline);
+			//}
+		//});
+	}
+function stringify (theOutline) { //returns the opmltext for the outline
+	var opmltext = opmlToJs.opmlify (theOutline);
+	return (opmltext);
 	}
