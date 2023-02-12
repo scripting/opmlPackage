3ad9311df426845d8109f05dd630bf497ea5a329
Dave Winer <dave.winer@gmail.com>
2023-02-11 20:37:52 -0500

    .

--- client/opml.js
+++ client/opml.js
@@ -8,6 +8,35 @@ const opml = {
 	outlineToMarkdown //1/3/22 by DW
 	};
 
+function filledString (ch, ct) { //6/4/14 by DW
+	var s = "";
+	for (var i = 0; i < ct; i++) {
+		s += ch;
+		}
+	return (s);
+	}
+function encodeXml (s) { //7/15/14 by DW
+	//Changes
+		//12/14/15; 4:28:14 PM by DW
+			//Check for undefined, return empty string.
+	if (s === undefined) {
+		return ("");
+		}
+	else {
+		var charMap = {
+			'<': '&lt;',
+			'>': '&gt;',
+			'&': '&amp;',
+			'"': '&'+'quot;'
+			};
+		s = s.toString();
+		s = s.replace(/\u00A0/g, " ");
+		var escaped = s.replace(/[<>&"]/g, function(ch) {
+			return charMap [ch];
+			});
+		return escaped;
+		}
+	}
 function xmlCompile (xmltext) { //3/27/17 by DW
 	return ($($.parseXML (xmltext)));
 	}
